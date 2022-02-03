91359e96b13a7cfd273b93431d0df82756956861
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-02-03 10:21:10 -0500

    Change naming conventions and start working on finalScene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text OP1;
+    public Text OP2;
+    public InputField ResultField;
+    public Text MessageText;
+    public int MaxOp = 10;
+    public int MinOp = 1;
+    private int _op1Val,_op2Val;
+
+
+    void Start()
+    {
+        ResetGame();
+    }
+
+    public void VerifyResult()
+    {
+        Debug.Log("VerifyResult");
+
+        int value = int.Parse(ResultField.text);
+        
+        if (value == _op1Val + _op2Val)
+        {
+            MessageText.text = "OK";
+        } else
+        {
+            MessageText.text = "NOK";
+            ResultField.text = "";
+        }
+
+        ResultField.ActivateInputField();
+    }
+
+    private void ResetGame()
+    {
+        _op1Val = Random.Range(MinOp, MaxOp);
+        _op2Val = Random.Range(MinOp, MaxOp);
+        OP1.text = _op1Val.ToString();
+        OP2.text = _op2Val.ToString();
+        MessageText.text = "";
+        ResultField.ActivateInputField();
+    }
+}
